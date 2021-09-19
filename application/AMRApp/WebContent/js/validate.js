e5ba5c8cfed04383194a32281a6b1186219c7e7c
Kanika Bagri <66821826+kanika1598@users.noreply.github.com>
2021-09-19 14:23:58 +0530

    Validations added

--- application/AMRApp/WebContent/js/validate.js
+++ application/AMRApp/WebContent/js/validate.js
@@ -1,24 +1,4 @@
-//function validateOrganizeMeetingForm(event){
-//	
-//	event.preventDefault();
-//	
-//	
-//	let meetingTitle = document.getElementById("meetingTitle").value;
-//	let meetingDate = document.getElementById("meetingDate").value;
-//	let startTime = document.getElementById("startTime").value;
-//	let endTime = document.getElementById("endTime").value;
-//	let duration = document.getElementById("duration").value;
-//	
-//	let searchRooms = document.getElementById("searchRooms");
-//	
-//	if(meetingType && meetingTitle && meetingDate && startTime && endTime)
-//		{
-//			searchRooms.removeAttribute("disabled")
-//		}
-//	
-//}
-
-
+//------------------validate the type of meeting---------------------------
 function validateMeetingType()
 {
 	let meetingType = document.getElementById("meetingType").value;
@@ -26,17 +6,28 @@ function validateMeetingType()
 		{
 			document.getElementById("errorMeetingType").innerHTML = "Kindly select the meeting type"
 		}
+	else{
+		document.getElementById("errorMeetingType").innerHTML = ""
+	}
+	
 }
 
+
+//------------------validate the title of meeting---------------------------
 function validateMeetingTitle(){
 	let meetingTitle = document.getElementById("meetingTitle").value;
 	if(meetingTitle=="")
 		{
 		document.getElementById("errorMeetingTitle").innerHTML = "Kindly give the meeting title"
 		}
+	else{
+		document.getElementById("errorMeetingTitle").innerHTML = ""
+	}
+	
 }
 
 
+//------------------validate the date of meeting---------------------------
 function validateMeetingDate(){
 	let meetingDate = document.getElementById("meetingDate").value;
 	let date = new Date()
@@ -46,13 +37,69 @@ function validateMeetingDate(){
 	   if (selectedDate < now) {
 		   document.getElementById("errorMeetingDate").innerHTML = "Date must be in the future"
 	   }
+	   else{
+		   document.getElementById("errorMeetingDate").innerHTML = ""
+	   }
 }
 
+
+//------------------validate the time of meeting---------------------------
 function validateTime(){
 	
 	let startTime = document.getElementById("startTime").value;
-	let date = new Date()
-	var nowTime = date.getTime()
+	let endTime = document.getElementById("endTime").value;
 	
-	let selectedTime
+	let[startTimeHours, startTimeMinutes] = startTime.split(":");
+	let[endTimeHours, endTimeMinutes] = endTime.split(":");
+	
+	let validationFailed = false;
+	document.getElementById('timeError').innerHTML = '';
+
+	if (startTime === '' || endTime === '') return
+
+	if (startTimeHours > endTimeHours) {
+		validationFailed = true;
+	}
+	else if (startTimeHours === endTimeHours && startTimeMinutes >= endTimeMinutes) {
+		validationFailed = true;
+	}
+
+	if (validationFailed) {
+		document.getElementById('timeError').innerHTML = 'Start time should always be less than end time!';
+	}
+	
+	//------------------Display the duration of meeting---------------------------
+	let durationHrsMin = ``
+	let hours;
+	let minutes;
+	if(endTimeHours > startTimeHours){
+		hours = endTimeHours - startTimeHours
+		
+		if (hours>1){
+			durationHrsMin += `${hours} Hours `
+		}
+		else if (hours == 1){
+			durationHrsMin += `${hours} Hour `
+		}
+	}
+	
+	if(endTimeMinutes > startTimeMinutes){
+		minutes = endTimeMinutes - startTimeMinutes
+		
+		if (minutes==1){
+			durationHrsMin += `${minutes} Minute`
+		}
+		else if (minutes > 1){
+			durationHrsMin += `${minutes} Minutes`
+		}
+		else {
+			durationHrsMin += " "
+		}
+	}
+
+	if (!validationFailed)
+	{
+		document.getElementById("durationHrsMin").innerHTML = durationHrsMin
+	}
+	
 }
