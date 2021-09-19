4d38fe98402558df1759fda528ded4f67774df35
DharmeshSoni1998 <89634547+DharmeshSoni1998@users.noreply.github.com>
2021-09-19 14:40:24 +0530

    UserFactory Uploaded

---
+++ application/AMRApp/src/main/factory/UserFactory.java
@@ -0,0 +1,13 @@
+
+package com.AMRApp.factory;
+
+import com.AMRApp.beans.User;
+
+public class UserFactory {
+
+	public static User createObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
