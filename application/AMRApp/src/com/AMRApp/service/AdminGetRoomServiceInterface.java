b74412c1e2b96f91c37749d0bdb09d03f2322e4b
ANANYA7566 <36131534+ANANYA7566@users.noreply.github.com>
2021-09-26 15:03:02 +0530

    Author Added

--- application/AMRApp/src/com/AMRApp/service/AdminGetRoomServiceInterface.java
+++ application/AMRApp/src/com/AMRApp/service/AdminGetRoomServiceInterface.java
@@ -1,5 +1,10 @@
 package com.AMRApp.service;
-
+/**
+ * @author Shivani Sharma
+ * @author Ananya Dwivedi
+ * @author Dharmesh Soni
+ *
+ */
 import java.util.List;
 
 import com.AMRApp.beans.MeetingRoom;
